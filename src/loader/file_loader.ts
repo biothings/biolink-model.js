d2430344d9c303d9838e0b6cf3464f68dcd4a410
jiwen xin <kevinxin@scripps.edu>
2021-02-23 14:52:44 -0800

    feat: :sparkles: add file loader to load from user provided spec

---
+++ src/loader/file_loader.ts
@@ -0,0 +1,13 @@
+import Loader from './base_loader';
+import util from 'util';
+import fs from 'fs';
+const readFile = util.promisify(fs.readFile);
+import { BioLinkJSON } from '../types/types'
+
+
+export default class FileLoader extends Loader {
+    async load(input) {
+        const file = await readFile(input, { encoding: 'utf8' });
+        return this.yaml2json(file) as BioLinkJSON;
+    }
+}
