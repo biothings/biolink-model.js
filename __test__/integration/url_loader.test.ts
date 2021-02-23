bc5603645e7c0e0f9ffac4f40a701c008cc62fc2
jiwen xin <kevinxin@scripps.edu>
2021-02-23 14:54:29 -0800

    test: :white_check_mark: add test for url loader

---
+++ __test__/integration/url_loader.test.ts
@@ -0,0 +1,11 @@
+import URLLoader from '../../src/loader/url_loader';
+import path from 'path';
+
+describe("Test URL Loader", () => {
+    test("BioLink yaml should be read from data/ folder and convert to json", async () => {
+        const loader = new URLLoader();
+        const res = await loader.load('https://raw.githubusercontent.com/biolink/biolink-model/master/biolink-model.yaml');
+        expect(res).toHaveProperty('id', 'https://w3id.org/biolink/biolink-model');
+        expect(res).toHaveProperty('name', 'Biolink-Model')
+    })
+})
