9d118558e3056e38ec8334fa706551bf6876bdc0
jiwen xin <kevinxin@scripps.edu>
2021-02-23 14:52:23 -0800

    feat: :sparkles: add default loader from spec stored within package

---
+++ src/loader/default_loader.ts
@@ -0,0 +1,14 @@
+import Loader from './base_loader';
+import util from 'util';
+import path from 'path';
+import fs from 'fs';
+const readFile = util.promisify(fs.readFile);
+import { BioLinkJSON } from '../types/types'
+
+
+export default class DefaultLoader extends Loader {
+    async load() {
+        const file = await readFile(path.resolve(__dirname, '../../data/biolink.yaml'), { encoding: 'utf8' });
+        return this.yaml2json(file) as BioLinkJSON;
+    }
+}
