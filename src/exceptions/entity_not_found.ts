0d465d6e46ee316fc13dde2d5684743f2dbe95cb
jiwen xin <kevinxin@scripps.edu>
2021-02-24 11:29:35 -0800

    refactor: :recycle: add EntityNotFound error

---
+++ src/exceptions/entity_not_found.ts
@@ -0,0 +1,7 @@
+export default class EntityNotFoundError extends Error {
+    constructor(message = "Entity is not in the tree") {
+        super();
+        this.name = "EntityNotFound";
+        this.message = message;
+    }
+}
