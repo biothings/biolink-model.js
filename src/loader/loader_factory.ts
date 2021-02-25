9478d02a355eb61e38e71c643fb0919d5868f67f
jiwen xin <kevinxin@scripps.edu>
2021-02-24 16:22:52 -0800

    refactor: :recycle: use factory pattern to design loader entry point

---
+++ src/loader/loader_factory.ts
@@ -0,0 +1,23 @@
+import DefaultLoader from './default_loader';
+import FileLoader from './file_loader';
+import UrlLoader from './url_loader';
+import fs from 'fs';
+import { LoaderClass } from '../types/types'
+
+const loader = (source: string = undefined): LoaderClass => {
+    if (typeof source === "undefined") {
+        return new DefaultLoader();
+    }
+    if (source.startsWith('http://') || source.startsWith('https://')) {
+        return new UrlLoader();
+    }
+    try {
+        if (fs.lstatSync(source).isFile()) {
+            return new FileLoader();
+        }
+    } catch (e) {
+        throw new Error("Your input is invalid. It should be undefined or a valid url or a valid file path.")
+    }
+}
+
+export default loader;
