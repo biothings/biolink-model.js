44b47d51e9761082c9cd39bcd0012c9d55d52f76
jiwen xin <kevinxin@scripps.edu>
2021-03-04 09:46:02 -0800

    test: :white_check_mark: update tests for Slot class

---
+++ __test__/unittest/slot_object.test.ts
@@ -0,0 +1,83 @@
+import Slot from '../../src/object/slot_object';
+
+describe("Test Slot Object", () => {
+    describe("Test inverse method", () => {
+        test("inverse property is correctly retreived if provided", () => {
+            const s = new Slot("negatively_regulates", { inverse: "negatively regulated by" });
+            expect(s.inverse).toEqual("negatively_regulated_by");
+        })
+
+        test("inverse property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.inverse).toBeUndefined;
+        })
+    })
+
+    describe("Test domain method", () => {
+        test("domain property is correctly retreived if provided", () => {
+            const s = new Slot("regulates", { domain: "physical essence or occurrent" });
+            expect(s.domain).toEqual("PhysicalEssenceOrOccurrent");
+        })
+
+        test("domain property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.domain).toBeUndefined;
+        })
+    })
+
+    describe("Test range method", () => {
+        test("range property is correctly retreived if provided", () => {
+            const s = new Slot("regulates", { range: "physical essence or occurrent" });
+            expect(s.range).toEqual("PhysicalEssenceOrOccurrent");
+        })
+
+        test("range property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.range).toBeUndefined;
+        })
+    })
+
+    describe("Test exact_mapping method", () => {
+        test("exact_mapping property is correctly retreived if provided", () => {
+            const s = new Slot("regulates", { exact_mapping: ["GO:regulates"] });
+            expect(s.exact_mapping).toEqual(["GO:regulates"]);
+        })
+
+        test("exact_mapping property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.exact_mapping).toBeUndefined;
+        })
+    })
+
+    describe("Test close_mapping method", () => {
+        test("close_mapping property is correctly retreived if provided", () => {
+            const s = new Slot("regulates", { close_mapping: ["GO:regulated_by", "RO:0002334"] });
+            expect(s.close_mapping).toEqual(["GO:regulated_by", "RO:0002334"]);
+        })
+
+        test("close_mapping property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.close_mapping).toBeUndefined;
+        })
+    })
+
+    describe("Test narrow_mapping method", () => {
+        test("narrow_mapping property is correctly retreived if provided", () => {
+            const s = new Slot("regulates", { narrow_mapping: ["WIKIDATA_PROPERTY:P128", "CHEMBL.MECHANISM:modulator"] });
+            expect(s.narrow_mapping).toEqual(["WIKIDATA_PROPERTY:P128", "CHEMBL.MECHANISM:modulator"]);
+        })
+
+        test("narrow_mapping property is undefined if not provided", () => {
+            const s = new Slot("negatively_regulates", {});
+            expect(s.narrow_mapping).toBeUndefined;
+        })
+    })
+
+    describe("Test add_child method", () => {
+        test("add child function is correctly performed", () => {
+            const s = new Slot("regulates", { narrow_mapping: ["WIKIDATA_PROPERTY:P128", "CHEMBL.MECHANISM:modulator"] });
+            s.addChild("negatively regulates")
+            expect(s.children).toEqual(["negatively_regulates"]);
+        })
+    })
+})
