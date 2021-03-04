3547a1d3c700aeb695c7df267d96166dcf35af22
jiwen xin <kevinxin@scripps.edu>
2021-03-04 09:14:31 -0800

    refactor: :recycle: update typescript interfacces

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,31 +1,56 @@
-export interface BioLinkClassObject {
+export interface BioLinkBase {
   is_a?: string;
   description?: string;
-  id_prefixes?: string[];
-  exact_mapping?: string[];
-  slots?: string[];
   [propName: string]: any;
 }
 
-export interface BioLinkClassesObject {
-  [propName: string]: BioLinkClassObject;
+export interface BioLinkClass extends BioLinkBase {
+  id_prefixes?: string[];
+  exact_mapping?: string[];
+  slots?: string[];
 }
 
-export interface BioLinkEntityObject {
+export interface BioLinkBaseObject {
+  [propName: string]: BioLinkBase;
+}
+
+export interface BioLinkClassesObject {
+  [propName: string]: BioLinkClass;
+}
+
+export interface BioLinkObject {
   parent: string;
   children: string[];
   description: string;
-  id_prefixes: string[];
   name: string;
   addChild(child: string): void;
+}
+
+export interface BioLinkObjects {
+  [propName: string]: BioLinkObject
+}
+
+export interface BioLinkEntityObject extends BioLinkObject {
+  id_prefixes: string[];
+}
+
+export interface BioLinkSlotObject extends BioLinkObject {
+  domain: string;
+  range: string;
+  exact_mapping: string[];
+  close_mapping: string[];
+  narrow_mapping: string[];
 }
 
 export interface BioLinkEntitiesObject {
   [propName: string]: BioLinkEntityObject;
 }
 
-interface BioLinkSlotObject {
-  description?: string;
+export interface BioLinkSlotsObject {
+  [propName: string]: BioLinkSlotObject;
+}
+
+export interface BioLinkSlot extends BioLinkBase {
   domain?: string;
   range?: string;
   exact_mapping?: string[];
@@ -34,23 +59,36 @@ interface BioLinkSlotObject {
   [propName: string]: any;
 }
 
-interface BioLinkSlotsObject {
-  [propName: string]: BioLinkSlotObject;
+export interface BioLinkSlots {
+  [propName: string]: BioLinkSlot;
 }
 
 export interface BioLinkJSON {
   classes: BioLinkClassesObject;
-  slots: BioLinkSlotsObject;
+  slots: BioLinkSlots;
   [propName: string]: any;
 }
 
-export interface BioLinkClassTreeObject {
-  entities: BioLinkEntitiesObject;
-  objects: BioLinkClassesObject;
+export interface BaseTreeObject {
+  objects: BioLinkObjects;
   construct(): void;
+  getDescendants(name: string): BioLinkObject[];
+  getAncestors(name: string): BioLinkObject[];
+  getPath(downstreamNode: string, upstreamNode: string): BioLinkObject[];
+}
+
+export interface BioLinkClassTreeObject extends BaseTreeObject {
+  objects: BioLinkEntitiesObject;
   getDescendants(name: string): BioLinkEntityObject[];
   getAncestors(name: string): BioLinkEntityObject[];
   getPath(downstreamNode: string, upstreamNode: string): BioLinkEntityObject[];
+}
+
+export interface BioLinkSlotTreeObject extends BaseTreeObject {
+  objects: BioLinkSlotsObject;
+  getDescendants(name: string): BioLinkSlotObject[];
+  getAncestors(name: string): BioLinkSlotObject[];
+  getPath(downstreamNode: string, upstreamNode: string): BioLinkSlotObject[];
 }
 
 export interface LoaderClass {
@@ -61,5 +99,6 @@ export interface BioLinkModule {
   load(source: string | undefined): void;
   loadSync(source: string | undefined): void;
   classTree: BioLinkClassTreeObject;
+  slotTree: BioLinkSlotTreeObject;
   biolinkJSON: BioLinkJSON;
 }
