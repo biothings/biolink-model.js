ae5dc2ef2517486da0cb9845e5b6bbaea4058e5a
jiwen xin <kevinxin@scripps.edu>
2021-04-26 21:22:43 -0700

    fix: :bug: change NCBIGENE to NCBIGene

--- __test__/unittest/entity_object.test.ts
+++ __test__/unittest/entity_object.test.ts
@@ -3,9 +3,9 @@ import Entity from '../../src/object/entity_object';
 describe("Test EntityObject class", () => {
     test("If input entity is Gene, should add additonal ID prefixes in addtion to provided ones", () => {
         const entity = new Entity("Gene", {
-            "id_prefixes": ["NCBIGENE"]
+            "id_prefixes": ["NCBIGene"]
         })
-        expect(entity.id_prefixes).toContain("NCBIGENE");
+        expect(entity.id_prefixes).toContain("NCBIGene");
         expect(entity.id_prefixes).toContain("UMLS");
         expect(entity.id_prefixes).toContain("SYMBOL");
         expect(entity.id_prefixes).toContain("OMIM");
