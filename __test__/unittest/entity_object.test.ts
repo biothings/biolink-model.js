3d09821d88e9a35aac693754e7127d4a8459daac
tokebe <43009413+tokebe@users.noreply.github.com>
2021-07-28 19:00:16 -0400

    style: return tab size to 2 (sorry)

--- __test__/unittest/entity_object.test.ts
+++ __test__/unittest/entity_object.test.ts
@@ -1,46 +1,46 @@
 import Entity from '../../src/object/entity_object';
 
 describe("Test EntityObject class", () => {
-    test("If input entity is Gene, should add additonal ID prefixes in addtion to provided ones", () => {
-        const entity = new Entity("Gene", {
-            "id_prefixes": ["NCBIGene"]
-        })
-        expect(entity.id_prefixes).toContain("NCBIGene");
-        expect(entity.id_prefixes).toContain("UMLS");
-        expect(entity.id_prefixes).toContain("SYMBOL");
-        expect(entity.id_prefixes).toContain("OMIM");
+  test("If input entity is Gene, should add additonal ID prefixes in addtion to provided ones", () => {
+    const entity = new Entity("Gene", {
+      "id_prefixes": ["NCBIGene"]
     })
+    expect(entity.id_prefixes).toContain("NCBIGene");
+    expect(entity.id_prefixes).toContain("UMLS");
+    expect(entity.id_prefixes).toContain("SYMBOL");
+    expect(entity.id_prefixes).toContain("OMIM");
+  })
 
-    test("If input entity is ChemicalSubstance, should add additonal ID prefixes in addtion to provided ones", () => {
-        const entity = new Entity("ChemicalSubstance", {
-            "id_prefixes": ["CHEMBL.COMPOUND"]
-        })
-        expect(entity.id_prefixes).toContain("CHEMBL.COMPOUND");
-        expect(entity.id_prefixes).toContain("UMLS");
+  test("If input entity is ChemicalSubstance, should add additonal ID prefixes in addtion to provided ones", () => {
+    const entity = new Entity("ChemicalSubstance", {
+      "id_prefixes": ["CHEMBL.COMPOUND"]
     })
+    expect(entity.id_prefixes).toContain("CHEMBL.COMPOUND");
+    expect(entity.id_prefixes).toContain("UMLS");
+  })
 
-    test("If input entity is SmallMolecule, should add additonal ID prefixes in addtion to provided ones", () => {
-        const entity = new Entity("SmallMolecule", {
-            "id_prefixes": ["CHEMBL.COMPOUND"]
-        })
-        // expect(entity.id_prefixes).toContain("CHEMBL.COMPOUND");
-        expect(entity.id_prefixes).toContain("UMLS");
+  test("If input entity is SmallMolecule, should add additonal ID prefixes in addtion to provided ones", () => {
+    const entity = new Entity("SmallMolecule", {
+      "id_prefixes": ["CHEMBL.COMPOUND"]
     })
+    // expect(entity.id_prefixes).toContain("CHEMBL.COMPOUND");
+    expect(entity.id_prefixes).toContain("UMLS");
+  })
 
-    test("If input entity is Disease, should add additonal ID prefixes in addition to provided ones", () => {
-        const entity = new Entity("Disease", {
-            "id_prefixes": ["KEGG"]
-        })
-        expect(entity.id_prefixes).toHaveLength(2);
-        expect(entity.id_prefixes).toContain("KEGG");
-        expect(entity.id_prefixes).toContain("GARD");
+  test("If input entity is Disease, should add additonal ID prefixes in addition to provided ones", () => {
+    const entity = new Entity("Disease", {
+      "id_prefixes": ["KEGG"]
     })
+    expect(entity.id_prefixes).toHaveLength(2);
+    expect(entity.id_prefixes).toContain("KEGG");
+    expect(entity.id_prefixes).toContain("GARD");
+  })
 
-    test("If input entity is not Gene or ChemicalSubstance, should just return provided ones", () => {
-        const entity = new Entity("PhenotypicFeature", {
-            "id_prefixes": ["HP"]
-        })
-        expect(entity.id_prefixes).toHaveLength(1);
-        expect(entity.id_prefixes).toContain("HP");
+  test("If input entity is not Gene or ChemicalSubstance, should just return provided ones", () => {
+    const entity = new Entity("PhenotypicFeature", {
+      "id_prefixes": ["HP"]
     })
+    expect(entity.id_prefixes).toHaveLength(1);
+    expect(entity.id_prefixes).toContain("HP");
+  })
 })
